2a66a301034b813dddae29830b15f53a97754cee
Pigalito <Pigalito@users.noreply.github.com>
2020-12-02 17:14:03 +0700

    Update variables.tf

--- terraform/services/eventbridge/rules/variables.tf
+++ terraform/services/eventbridge/rules/variables.tf
@@ -1,8 +1,16 @@
-variable "event_pattern" {}
+variable "event_pattern" {
+  default = ""
+}
 variable "name" {}
 variable "description" {}
 variable "event_bus_name" {}
 variable "target_arn" {}
+variable "schedule_expression" {
+  default = ""
+}
 variable "role_arn" {
+  default = null
+}
+variable "input_transformer" {
   default = null
 }
