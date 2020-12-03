580c1a43d3a305e5242a968a1fb475b6d15266b1
DavidHackettPomelo <david.h@pomelofashion.com>
2020-12-03 10:15:08 +0700

    Shrink kinesis buffer for faster testing

--- terraform/services/kinesis/kinesis.tf
+++ terraform/services/kinesis/kinesis.tf
@@ -5,5 +5,7 @@ resource "aws_kinesis_firehose_delivery_stream" "eventbridge_s3_firehose_stream"
   extended_s3_configuration {
     role_arn   = var.firehose_role_arn
     bucket_arn = var.events_bucket_arn
+    buffer_size = 1
+    buffer_interval = 60
   }
 }
